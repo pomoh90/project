b7a809a1317ac2872bf1f1d2574b1dd4aab2065f
Roman <dacaball14121990@gmail.com>
2021-08-10 19:59:13 +0300

    циклы

---
+++ project_1/js/cicles.js
@@ -0,0 +1,24 @@
+"use strict";
+
+
+let num = 50;
+
+// while (num < 55) {     // увеличение на 1
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+for ( let i = 1; i < 10; i++) {
+    if (i === 6) {
+        //break;
+        continue;
+    }
+    
+    console.log(i);
+}
