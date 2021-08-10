169238ebef2921703f6ee07f3a83e451b1a7abe9
Roman <dacaball14121990@gmail.com>
2021-08-10 19:02:33 +0300

    third commit

--- project_1/js/script.js
+++ project_1/js/script.js
@@ -1,4 +1,23 @@
 "use strict";
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres:[],
+    privat: false
+};
+
+const a = prompt('Один из последний просмотренных фильмов?',''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последний просмотренных фильмов?',''),
+      d = prompt('На сколько оцените его?','');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
 
 
 
