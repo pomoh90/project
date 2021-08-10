002bd7f5191087d8ec90d95d27aad9bdcdc21930
Roman <dacaball14121990@gmail.com>
2021-08-10 18:43:56 +0300

    second

--- project_1/js/script.js
+++ project_1/js/script.js
@@ -1,15 +1,17 @@
 "use strict";
 
 
-let incr = 10,
-    decr = 10;
 
 
-incr++;
-decr--;
+// let incr = 10,
+//     decr = 10;
 
-console.log(incr);
-console.log(decr);
+
+// incr++;
+// decr--;
+
+// console.log(incr);
+// console.log(decr);
     
 
 
