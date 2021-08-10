20eeca006df1f0bdecd4522a4606179c6fdd74cf
Roman <dacaball14121990@gmail.com>
2021-08-10 16:45:14 +0300

    first commit

---
+++ project_1/js/script.js
@@ -0,0 +1,67 @@
+"use strict";
+
+
+let incr = 10,
+    decr = 10;
+
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5` );
+
+// const user = 'Ivan';
+// alert(`Hi,${user}`);
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+
+// };
+
+// console.log(obj.name);
+
+// let arr = ['plum.png','orange.jpeg',6,'apple.png',{}];
+
+// const res = confirm("ti tyt?");
+// console.log(res);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('имя','');
+// answers[1] = prompt('возраст','');
+// answers[2] = prompt('фамилия','');
+
+// console.log(typeof(answers));
